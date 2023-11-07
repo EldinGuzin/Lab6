fec71cacca3397a4c421c1fa7d75ff992c9d62aa
EldinGuzin <eldin.guzin@stu.ibu.edu.ba>
2023-11-07 13:31:05 +0100

    finished first 4 tasks

--- src/hw/Main.java
+++ src/hw/Main.java
@@ -7,20 +7,68 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//1
+		
 		
 		
 		
 		int[] values = {6,5,8,7,11};
-		System.out.println("smallest: " + smallest(values));
-		
-
+		//System.out.println("smallest: " + smallest(values));
+		//System.out.println("Index of the smallest: " + indexOfTheSmallest(values));
+		System.out.println(indexOfTheSmallestStartingFrom(values, 2));
 	}
 
+	//1
 	public static int smallest(int[] array) {
 		
 		Arrays.sort(array);
 		return array[0];
 		
 }
+	
+	//2
+	
+	public static int indexOfTheSmallest(int[] array) {
+		
+		int smallest = array[0];
+		int indexOfTheSmallest = 0;
+		
+		for(int i = 1; i < array.length; i++) {
+			
+			if(array[i] < smallest) {
+				smallest = array[i];
+				indexOfTheSmallest = i;
+			}
+			
+			
+		}
+		
+		return indexOfTheSmallest;
+		
 	}
+	
+	
+	//3
+	public static int indexOfTheSmallestStartingFrom(int[] array, int index) {
+		
+		int smallest = array[index];
+		int result = index;
+		
+		for(int i = index; i < array.length; i++) {
+			
+			if(array[i] < smallest) {
+				
+				smallest = array[i];
+				result = i;
+				
+				
+			}
+
+			}
+		return result;
+	}
+	
+	
+	
+	
+	
+		}
